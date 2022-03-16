de87a44ecd5cc54bdbe520de291c9ef4a4982f30
FMrtnz <fab.mrtnz22@gmail.com>
2022-03-16 14:53:22 +0000

    Set the challenge Set the input to select the region available in the table Display table Display correlation

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -3,30 +3,38 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 
-# Set a title to the page
-st.title('Hello Wilders, welcome to my application!')
-# Set a text
-st.write("I enjoy to discover stremalit possibilities")
+# Set a container
+with st.container():
+    # Set a title to the page
+    st.title('WCS Challenge : Cars analyse')
 
-# Get the CSV link
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-# READ the CSV link with pandas then display
-df_weather = pd.read_csv(link)
-df_weather
+    # Get the CSV link
+    link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+    # READ the CSV link with pandas then display
+    df_cars = pd.read_csv(link)
+
+    regions_list = df_cars.groupby(["continent"]).count().index.values
+
+    # Set an input to choose region
+    options = st.multiselect(
+     'By region(s) (US / Europe / Japan)',
+     regions_list
+    )
+    df_cars
 
 # set a chart
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+#st.line_chart(df_weather['MAX_TEMPERATURE_C'])
 
-# Set correlation then see the chart with
-viz_correlation = sns.heatmap(df_weather.corr(),
+with st.container():
+    # Set correlation then see the chart with
+    viz_correlation = sns.heatmap(df_cars.corr(),
 								center=0,
 								cmap = sns.color_palette("vlag", as_cmap=True)
 								)
-
-# We replace plt.show() by st.pyplot()
-st.pyplot(viz_correlation.figure)
+    # We replace plt.show() by st.pyplot()
+    st.pyplot(viz_correlation.figure)
 
 # Set an input
-name = st.text_input("Please give me your name:")
-name_length = len(name)
-st.write("Your name has ",name_length, "characters")
+# name = st.text_input("Please give me your name:")
+# name_length = len(name)
+# st.write("Your name has ",name_length, "characters")
