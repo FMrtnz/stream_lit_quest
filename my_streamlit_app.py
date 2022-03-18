3dab7443ecfee3542edf9c5d578376d680778de9
FMrtnz <fab.mrtnz22@gmail.com>
2022-03-18 12:27:02 +0000

    Create function to generate scatter plot with x-axis and list of y-axis as params

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -76,46 +76,44 @@ st.pyplot(viz_correlation.figure)
 # Set list of colors for each country
 colors = {regions_list[0]: "red" , regions_list[1]:"orange", regions_list[2]:"green"}
 
+# define a function to create differents plots scatter
+def loop_plot_scatter(x_axis, y_axis_cols):
+    for col in y_axis_cols:
+        fig, ax = plt.subplots()
+        for region in labels_countries:
+            # Select the region for each axe
+            df_filtered[df_filtered['continent'] == region]\
+            # Use plot function from panda to set | kind is a param to set plot type \
+            .plot(kind="scatter",\
+            # ax allows us to association to a fig position\
+            ax = ax, \
+            # Setting X and Y axis\
+            x=x_axis, y=col, ylabel=col, figsize= (10, 3), \
+            # Define color for axe with the color list created \
+            color = colors[region])
+        ax.legend(labels_countries)
+        st.pyplot(fig)
+
 #Loop to create plot with mpg as x-axis
 #Define list of plot to create
 cols = ["cubicinches","hp","weightlbs","time-to-60"]
 x_axis="mpg"
-for col in cols:
-    fig, ax = plt.subplots()
-    for region in labels_countries:
-        df_filtered[df_filtered['continent'] == region].plot(kind="scatter",ax = ax, x=x_axis, y=col, ylabel=col, figsize= (10, 3), color = colors[region])
-    ax.legend(labels_countries)
-    st.pyplot(fig)
+loop_plot_scatter(x_axis, cols)
 
 #Loop to create plot with x-axis cylinders
 cols_2 = ["mpg", "cubicinches", "hp","weightlbs","time-to-60"]
 x_axis="cylinders"
-for col in cols_2:
-    fig, ax = plt.subplots()
-    for region in labels_countries:
-        df_filtered[df_filtered['continent'] == region].plot(kind="scatter",ax = ax, x=x_axis, y=col, ylabel=col, figsize= (10, 3), color = colors[region])
-    ax.legend(labels_countries)
-    st.pyplot(fig)
+loop_plot_scatter(x_axis, cols_2)
 
 #Loop to create plot with x-axis cubicinches
 cols_3 = ["hp","weightlbs","time-to-60"]
 x_axis = "cubicinches"
-for col in cols_3:
-    fig, ax = plt.subplots()
-    for region in labels_countries:
-        df_filtered[df_filtered['continent'] == region].plot(kind="scatter",ax = ax, x=x_axis, y=col, ylabel=col, figsize= (10, 3), color = colors[region])
-    ax.legend(labels_countries)
-    st.pyplot(fig)
+loop_plot_scatter(x_axis, cols_3)
 
 #Loop to create plot with x-axis hp
 cols_4 = ["weightlbs","time-to-60"]
 x_axis = "hp"
-for col in cols_4:
-    fig, ax = plt.subplots()
-    for region in labels_countries:
-        df_filtered[df_filtered['continent'] == region].plot(kind="scatter",ax = ax, x=x_axis, y=col, ylabel=col, figsize= (10, 3), color = colors[region])
-    ax.legend(labels_countries)
-    st.pyplot(fig)
+loop_plot_scatter(x_axis, cols_4)
 
 #Loop to create plot with x-axis year
 cols_5 = ["mpg","time-to-60"]
